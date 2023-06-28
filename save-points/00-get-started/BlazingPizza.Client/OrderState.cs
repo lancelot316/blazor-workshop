33821ff464dd02db1674cc8282242ee74523bcb5
Lance Newman <lance.newman@elynxx.com>
2023-06-28 09:13:57 -0400

    refactored state management

---
+++ save-points/00-get-started/BlazingPizza.Client/OrderState.cs
@@ -0,0 +1,45 @@
+﻿using BlazingPizza;
+
+public class OrderState
+{
+    public bool ShowingConfigureDialog { get; private set; }
+    public Pizza ConfiguringPizza { get; private set; }
+    public Order Order { get; private set; } = new Order();
+
+    public void ShowConfigurePizzaDialog(PizzaSpecial special)
+    {
+        ConfiguringPizza = new Pizza()
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>()
+        };
+
+        ShowingConfigureDialog = true;
+    }
+
+   public void CancelConfiguringPizzaDialog()
+    {
+        ConfiguringPizza = null;
+        ShowingConfigureDialog = false;
+    }
+
+    public void ConfirmConfiguringPizzaDialog()
+    {
+        Order.Pizzas.Add(ConfiguringPizza);
+        ConfiguringPizza = null;
+
+        ShowingConfigureDialog = false;
+    }
+
+    public void RemoveConfiguredPizza(Pizza pizza)
+    {
+        Order.Pizzas.Remove(pizza);
+    }
+
+    public void ResetOrder()
+    {
+        Order = new Order();
+    }
+}
